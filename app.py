08ce9b143f0275e0db59e2d95e90df541216147f
mbuiop <milad.yari1377m@gmail.com>
2026-06-24 16:44:28 +0330

    Update app.py

--- app.py
+++ app.py
@@ -102,9 +102,9 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-BOT_TOKEN = "8195783182:AAH408rNKlNZYnnB_E65xA0dG6I_dGpUS7I"
+BOT_TOKEN = "8787172986:AAHtlVXWZTTFUrvWc0OcVI-CehKxkPmF7nA"
 ADMIN_ID = 327855654
-BOT_USERNAME = "@Maynir_Bot"
+BOT_USERNAME = "@ROBTTSAZE_bot"
 EXCHANGE_URL = "https://www.toobit.com/fa/r?i=5EQpCT"
 
 # ==================== لیست ۲۰۰+ ارز ====================
